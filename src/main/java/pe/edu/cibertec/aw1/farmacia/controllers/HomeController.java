1550b45fa956675ba4a0c27229c0e89d4bfd8e96
artmadeit <this.arthur.email@gmail.com>
2023-08-02 19:51:52 -0500

    feat: controller advice para year

--- src/main/java/pe/edu/cibertec/aw1/farmacia/controllers/HomeController.java
+++ src/main/java/pe/edu/cibertec/aw1/farmacia/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package pe.edu.cibertec.aw1.farmacia.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +12,7 @@ public class HomeController {
     
     // @RequestMapping(path = "/", method = RequestMethod.GET)
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("year", 2023);
+    public String home() {
         return "home";
     }
     
